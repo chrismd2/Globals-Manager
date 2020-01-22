402c3a6bd24819b6fe51c90a1aafd286bb8f4b2d
chrismd2 <53451424+chrismd2@users.noreply.github.com>
2020-01-22 08:51:11 -0600

    Switch Statements Added for CLI Options
    Added switch statements to handle option in the command line
         - One that handles entering developer mode or chooses user options
         - Enter developer mode by using d as an option
         - Make developer mode options by appending them to d option
              + ie dx

--- main.cpp
+++ main.cpp
@@ -1,37 +1,94 @@
+/*
+Name:		Christenson, Mark
+File:		main.cpp
+Description:	This program is to facilitate testing of other programs
+		User options can be defined in main
+    The main function is the last one
+    Developer test function
+      - Named devTest
+      - Second to last function
+      - Called in developer mode in CLI with x
+      - Can safely be edited to change its behavior for testing 
+*/
+
 #include <iostream>
-#include <string>
+#include <string.h>
+#include <stdio.h>
 
 #include "GLOBALS.cpp"
 
 using namespace std;
 
+//Shows passed arguments
+void showArgs(int argc, char const *argv[]){
+  if(argc >= 1){
+    cout << "All input arguments\n";
+    for(int i = 0; i < argc; i++){
+      cout <<"\t" << i << ": " << argv[i] << endl;
+    }
+  }
+}
+
+//Shows global variables
 void showKnownGlobals(){
   cout  << "Known Globals:"
-        << "\n\tBALANCE = "   <<  BALANCE
-        << "\n\tLENGTH = "    <<   LENGTH
-        << "\n\tFILENAME = "  << FILENAME
-        << "\n\tCOMPILED = "  << COMPILED
         << "\n\tDEBUG = "     <<    DEBUG
         << endl;
 }
 
-int main(int argc, char const *argv[]) {
-  showKnownGlobals();
+//Calls two funtions to update global variables and rewrite
+void update(){
   FetchGlobalVariables();
-  /*
-  if(DEBUG){
-    cout << "\nIn Main\n";
-    if(argc > 1){
-      cout << "All input arguments\n";
-      for(int i = 0; i < argc; i++){
-        cout <<"\t" << i << ": " << argv[i] << endl;
-      }
-    }
-    getUndefinedVariables();
-  }
-  */
   rewrite();
-  showKnownGlobals();
+}
+
+//Test function to be called for option x for developer mode
+void devTest(){
+  if(DEBUG){cout << "Developer's test function called\n";}
+}
+
+/*
+Developer Options - activated by leading option with d
+  - x: Calls devTest, a test function for developer to call with developer mode options
+  - d: displays arguments data by calling showArgs
+  - u: runs update
+
+User Options
+  - cases to be defined in switch statement
+*/
+int main(int argc, char const *argv[]) {
+  for(int i = 1; i < argc; i++){
+    //Define user options as a case of this switch statement
+    switch (argv[i][0]) {
+      case 'd':
+        if(DEBUG){cout << "developer mode case called\n";}
+        //This loop checks all options after developer mode is activated
+        for(int j = 1; j < strlen(argv[i]); j++){
+          //define developer options in this switch statement
+          switch (argv[i][j]) {
+            case 'x':
+              if(DEBUG){cout << "x case called\n";}
+              test();
+              break;
+            case 'd':
+              showArgs(argc, argv);
+              break;
+            case 'u':
+              if(DEBUG){cout << "Update case called\n";}
+              update();
+              break;
+            default:
+              cout << "ERROR: option is undefined\n";
+          }//End of developer switch statement
+        }
+        if(strlen(argv[i]) == 1){
+          cout << "ERROR: must choose an option\n";
+        }
+        break;
+      default:
+        cout << "ERROR: there is no defined case for argv #" << i << "\n";
+    }//End of switch statement that includes user options
+  }
 
   if(DEBUG){cout << "\nMain complete\n";}
 
